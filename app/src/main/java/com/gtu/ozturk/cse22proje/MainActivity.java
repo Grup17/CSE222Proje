b75038400ec2eade2a9701d69a02d906e61f1bfd
OSMAN ÖZTÜRK <osman.ozturk2016@gtu.edu.tr>
2018-04-16 18:07:01 +0300

    Initial Commit with empty activity

---
+++ app/src/main/java/com/gtu/ozturk/cse22proje/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gtu.ozturk.cse22proje;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
