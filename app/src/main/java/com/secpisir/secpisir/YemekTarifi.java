7ad5aec21185e2a1fae112f84fe7d833bf019792
simgesaricayir <simge_saricayir@hotmail.com>
2018-05-31 16:31:10 +0300

    Update YemekTarifi.java

--- app/src/main/java/com/secpisir/secpisir/YemekTarifi.java
+++ app/src/main/java/com/secpisir/secpisir/YemekTarifi.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class YemekTarifi extends AppCompatActivity {
+
     private static Yemek yemek;
 
     @Override
@@ -37,7 +40,7 @@ public class YemekTarifi extends AppCompatActivity {
 
     public void yemegiGecmiseEkle(View view) {
         if(YönetimSistemi.getCurrentKullanici()!=null) {
-            Kullanici k = YönetimSistemi.getKullanici();
+            YönetimSistemi.getCurrentKullanici().getGecmis().add(yemek.getIsim());
             //k.gecmiseEkle(yemek.getIsim());
             Intent intent = new Intent(this, MainScreen.class);
             startActivity(intent);
@@ -48,22 +51,34 @@ public class YemekTarifi extends AppCompatActivity {
     }
 
     public void yemegiFavorilereEkle(View view) {
-        if(YönetimSistemi.getCurrentKullanici()!=null) {
-            Kullanici k = YönetimSistemi.getKullanici();
-            k.favorilereEkle(yemek.getIsim());
+        if(YönetimSistemi.getCurrentKullanici()!=null &&
+                !YönetimSistemi.getCurrentKullanici().getKaraListe().contains(yemek.getIsim())) {
+            YönetimSistemi.getCurrentKullanici().getFavoriListe().add(yemek.getIsim());
+            //k.favorilereEkle(yemek.getIsim());
             Toast.makeText(getApplicationContext(), "Favorilere eklendi",Toast.LENGTH_LONG).show();
-        }else{
+        }
+        else if(YönetimSistemi.getCurrentKullanici().getFavoriListe().contains(yemek.getIsim())){
+            Toast.makeText(getApplicationContext(), "Eklemek istediğiniz yemek"+
+                    " zaten favorilerinizde mevcut.",Toast.LENGTH_SHORT).show();
+        }
+        else{
             Toast.makeText(getApplicationContext(), "Bu özellik yalnızca kayıtlı"+
                     " kullanıcılar içindir...",Toast.LENGTH_SHORT).show();
         }
     }
 
     public void yemegiKaralisteyeEkle(View view){
-        if(YönetimSistemi.getCurrentKullanici()!=null) {
-            Kullanici k = YönetimSistemi.getKullanici();
-            k.karaListeyeEkle(yemek.getIsim());
+        if(YönetimSistemi.getCurrentKullanici()!=null &&
+        !YönetimSistemi.getCurrentKullanici().getFavoriListe().contains(yemek.getIsim())) {
+            YönetimSistemi.getCurrentKullanici().getKaraListe().add(yemek.getIsim());
+          //  k.karaListeyeEkle(yemek.getIsim());
             Toast.makeText(getApplicationContext(), "Kara listeye eklendi",Toast.LENGTH_LONG).show();
-        }else{
+        }
+        else if(YönetimSistemi.getCurrentKullanici().getKaraListe().contains(yemek.getIsim())){
+            Toast.makeText(getApplicationContext(), "Eklemek istediğiniz yemek"+
+                    " zaten kara listede mevcut.",Toast.LENGTH_SHORT).show();
+        }
+        else{
             Toast.makeText(getApplicationContext(), "Bu özellik yalnızca kayıtlı"+
             " kullanıcılar içindir...",Toast.LENGTH_SHORT).show();
             }
